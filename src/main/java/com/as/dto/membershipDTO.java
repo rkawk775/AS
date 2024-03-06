566e8ef4b11c8fcbe5bd69d19c858f499ab38b9f
rkawk775 <dbswn08050@naver.com>
2024-03-06 09:42:23 +0900

    DTO

---
+++ src/main/java/com/as/dto/membershipDTO.java
@@ -0,0 +1,47 @@
+package com.as.dto;
+
+public class membershipDTO {
+	private String email;
+	private String pw;
+	private String name;
+	private String phone;
+	private int res_id;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getRes_id() {
+		return res_id;
+	}
+	public void setRes_id(int res_id) {
+		this.res_id = res_id;
+	}
+	@Override
+	public String toString() {
+		return "membershipDTO [email=" + email + ", pw=" + pw + ", name=" + name + ", phone=" + phone + ", res_id="
+				+ res_id + "]";
+	}
+	
+	
+}
