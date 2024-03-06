aa317087c4ef1ad1fecd55a3c8776d08d81d359a
HGR413 <rkfka413@naver.com>
2024-03-06 09:42:40 +0900

    reservationDTO

---
+++ reservationDTO.java
@@ -0,0 +1,11 @@
+public class reservationDTO {
+	private String asitem;
+	private Date res_date;
+	private String res_time;
+	private String res_name;
+	private String res_id;
+	private String phone;
+	private String email;
+	
+	
+}
